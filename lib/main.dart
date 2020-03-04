bdbb2032a5d73e92514e9bef0fc20b477faa0a6a
Sajan-poudel <sajanpoudel32@gmail.com>
2020-03-04 17:08:22 +0530

    socket

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'dart:core';
-
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:speech_recognition/speech_recognition.dart';
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'package:flutter_socket_io/socket_io_manager.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -26,10 +32,17 @@ class _HomePageState extends State<HomePage> {
   String resulttext = "";
   bool speakin = false;
   List<String> split;
-
+  SocketIO socketIO;
   @override
   void initState() {
     // TODO: implement initState
+    socketIO = SocketIOManager().createSocketIO(
+      'https://13.232.28.46:3000',
+      '/',
+    );
+    socketIO.init();
+
+    socketIO.connect();
     super.initState();
     _speechRecognization = SpeechRecognition();
     _speechRecognization.setAvailabilityHandler(
@@ -86,9 +99,9 @@ class _HomePageState extends State<HomePage> {
                         .listen(locale: "en_US")
                         .then((result) => print('$result'));
                 } else {
-                  if(resulttext.contains("get me to")){
+                  if (resulttext.contains("get me to")) {
                     split = resulttext.split("get me to");
-                  }else if(resulttext.contains("guide me to")){
+                  } else if (resulttext.contains("guide me to")) {
                     split = resulttext.split("guide me to");
                   }
                   //fetchlocation(split[1]);
@@ -109,9 +122,61 @@ class _HomePageState extends State<HomePage> {
               },
             ),
           ),
+          FloatingActionButton(
+            child: Text("data"),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Mapview()));
+            },
+          ),
         ],
       ),
     );
   }
 }
 
+class Mapview extends StatefulWidget {
+  Mapview({Key key}) : super(key: key);
+
+  @override
+  _MapviewState createState() => _MapviewState();
+}
+
+class _MapviewState extends State<Mapview> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(12.971599, 77.594566),
+          zoom: 15,
+        ),
+        layers: [
+          new TileLayerOptions(
+            urlTemplate: "https://apis.mapmyindia.com/advancedmaps/v1/ge79np57h6uwlfla4k4fzn2efrgoplnh/map_load?v=1.3"
+            // urlTemplate: "https://api.tiles.mapbox.com/v4/"
+            //     "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
+            // additionalOptions: {
+            //   'accessToken':
+            //       'pk.eyJ1Ijoic2FqYW4tcG91ZGVsIiwiYSI6ImNrMnZnbGw3ZTA0aTgzbG5xcTNpMDFzbHAifQ.Criu5-m3kWFRKo7vcZ3NYA',
+            //   'id': 'mapbox.streets',
+            // },
+          ),
+          new MarkerLayerOptions(
+            markers: [
+              new Marker(
+                width: 80.0,
+                height: 80.0,
+                point: new LatLng(12.971599, 77.594566),
+                builder: (ctx) => new Container(
+                  child: Icon(Icons.map),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
